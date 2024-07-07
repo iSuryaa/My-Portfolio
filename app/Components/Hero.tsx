1768209f2836cca4d73f372ecacc9d8bd680036c
suryaprakash <suryaprakash2461@gmail.com>
2024-07-07 11:17:57 +0530

    fitst commit

---
+++ app/Components/Hero.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+import MagicButton from './ui/MagicButton'
+import Link from 'next/link'
+import { FaLocationArrow } from 'react-icons/fa'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <h1>Hello World</h1>
+        <Spotlight className='-top-40 -left-10 md:top-20 md:left-36 h-screen' fill='white'/>
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple"/>
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue"/>
+        </div>
+        <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+    </div>
+
+    <div className='flex justify-center relative my-20 z-10'>
+      <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+          Dynamic web magic with Next.js
+        </h2>
+
+        <TextGenerateEffect 
+          className='text-center text-[40px] md:text-5xl lg:text-6xl'
+          words='Transforming concept with seemless user experience'/>
+
+          <p className='text-center tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'> Hi I'm Surya Prakash, a web developer, based in croaitia</p>
+          <Link href='#about' >
+           <MagicButton title='Show My Work' icon={<FaLocationArrow/>} position='right'/>
+          </Link>
+      </div>
+    </div>
+
+    
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Hero from "./Components/Hero";
+import {FloatingNav} from "./Components/ui/floating-navbar"
+import { FaHome } from "react-icons/fa";
+import Grid from "./Components/Grid";
+import RecentProjects from "./Components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "./Components/Clients";
+import Experience from "./Components/Experience";
+import Approach from "./Components/Approach";
+import Footer from "./Components/Footer";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+       <Hero/>
+       <Grid/>
+       <RecentProjects/>
+       <Clients/>
+       <Experience/>
+       <Approach/>
+       <Footer/>
+      </div>
+    </main>
+  );
+}
